07e221dce7d27762b462715103528f09ffbf76ca
sandg9896 <sandg9896@wrdsb.ca>
2020-10-09 12:35:46 +0000

    Same Number Summative

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * check to see if both arrays are the same or are not and output that to the user
+ * @ Gavin Sandhar 
  */
 public class Main {
 
@@ -9,7 +10,40 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // create array to represent first set of numbers
+    int[] first = new int[5];
+   
+    // ask user to enter 5 numbers    
+    System.out.println("Please enter the values for the first array:");
     
+    // create for loop for first set numbers
+    for(int i = 0; i < first.length; i++){
+      first[i] = input.nextInt();
+    }
+    
+    // create array to represent second set of numbers
+    int[] second = new int[5];
+
+    // ask user to enter 5 numbers    
+    System.out.println("Please enter the values for the second array:");
+    
+    // create for loop for second set numbers
+    for(int i = 0; i < second.length; i++){
+      second[i] = input.nextInt();
+    }
+    // create if else statement to determine if the values are the same or not
+    int i = 0;
+    if(first[i] == second[i]){
+      System.out.println("These arrays are the same");
+    }else{
+      // tell them the arrays aren't the same
+      System.out.println("These arrays are not the same");
+    }
   }
 }
+ 
+  
+
